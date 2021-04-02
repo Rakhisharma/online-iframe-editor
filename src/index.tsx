914b02c3d0f01b25066250893c805f89b64cdd0a
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-04-02 22:54:18 +0200

    move file to src folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from "react-dom";
 
-import App from "./src/Editor";
+import App from "./Editor";
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
