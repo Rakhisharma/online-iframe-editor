0dba88130ae2efc60e6a459720f08175a067a5c4
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-04-03 12:35:30 +0200

    Add prettier

--- src/Editor/index.tsx
+++ src/Editor/index.tsx
@@ -1,51 +1,46 @@
-import { useState } from "react";
-import { Grid, TextField, Typography} from "@material-ui/core";
+import { useState } from 'react';
+import { Grid, TextField, Typography } from '@material-ui/core';
 
 import useStyles from './styles';
 export default function App() {
+    const { root, header, input, resultContent } = useStyles();
+    const [iframeValue, setIframeValue] = useState('');
 
-  const { root, header, input, resultContent } = useStyles();
-  const [iframeValue, setIframeValue] = useState('');
+    const handleChange = (value: any) => {
+        setIframeValue(value);
+    };
 
-  const handleChange = (value: any) => {
-    setIframeValue(value);
-  };
-
-
-  return (
-    <>
-      <Grid container spacing={3} className={root}>
-        <Grid item xs={12}>
-          <Typography
-            variant="h4"
-            className={header}
-            >
-            {`Online <iframe> viewer`}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Paste your iframe snippet"
-            variant="outlined"
-            onChange={({ target }) => {
-              const value = target.value;
-              handleChange(value);
-            }}
-            InputProps={{
-              classes: { input }
-            }}
-            fullWidth
-            multiline
-          />
-        </Grid>
-        <Grid item xs className={resultContent}>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: iframeValue
-              }}
-            />
-        </Grid>
-      </Grid>
-    </>
-  );
+    return (
+        <>
+            <Grid container spacing={3} className={root}>
+                <Grid item xs={12}>
+                    <Typography variant="h4" className={header}>
+                        {`Online <iframe> viewer`}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        label="Paste your iframe snippet"
+                        variant="outlined"
+                        onChange={({ target }) => {
+                            const value = target.value;
+                            handleChange(value);
+                        }}
+                        InputProps={{
+                            classes: { input }
+                        }}
+                        fullWidth
+                        multiline
+                    />
+                </Grid>
+                <Grid item xs className={resultContent}>
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: iframeValue
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
 }
--- src/Editor/styles.ts
+++ src/Editor/styles.ts
@@ -1,31 +1,29 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const styles = makeStyles(() => ({
-  root: {
-    height: "100%",
-    width: '100%'
-  },
+    root: {
+        height: '100%',
+        width: '100%'
+    },
 
-  header: {
-    margin: '15px',
-    textAlign: 'center',
-    fontFamily: "UI-MONOSPACE"
-  },
+    header: {
+        margin: '15px',
+        textAlign: 'center',
+        fontFamily: 'UI-MONOSPACE'
+    },
 
-  input: {
-    minHeight: '180px',
-    borderColor: '#000',
-    border: "2px"
-  },
+    input: {
+        minHeight: '180px',
+        borderColor: '#000',
+        border: '2px'
+    },
 
-  resultContent: {
-    marginTop: '10px',
-    borderColor: '#000',
-    border: "4px",
-    minHeight: '500px'
-  }
+    resultContent: {
+        marginTop: '10px',
+        borderColor: '#000',
+        border: '4px',
+        minHeight: '500px'
+    }
 }));
 
 export default styles;
-
