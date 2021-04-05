c50adfde4b5067945dd4cd4ffcd0d60c679b0745
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-04-05 14:24:13 +0200

    Add Header Component

---
+++ src/Header/constants.ts
@@ -0,0 +1,21 @@
+const HEADER = 'Online <iframe> editor';
+
+const GITHUB_ISSUE =
+    'https://github.com/Rakhisharma/online-iframe-editor/issues';
+
+const REPORT_ISSUE = 'Report Issue';
+
+const TWITTER_LINK = 'https://twitter.com/atbrakhi';
+
+const CREATED_BY = 'Created by';
+
+const TWITTER_USER = 'atbrakhi';
+
+export {
+    HEADER,
+    GITHUB_ISSUE,
+    REPORT_ISSUE,
+    TWITTER_LINK,
+    CREATED_BY,
+    TWITTER_USER
+};
